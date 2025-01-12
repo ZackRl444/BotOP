40a33bc4a3c06ce91ac68b1bcb25daa7378252cc
ZackRl444 <zigzag140708@gmail.com>
2025-01-12 19:54:04 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+# Route pour télécharger le fichier
+@app.route('/download')
+def download_file():
+    # Remplace le chemin par le chemin correct vers ton fichier
+    file_path = '/workspace/inventory_backup.zip'
+    return send_file(file_path, as_attachment=True)
+
+# Lancer le serveur Flask sur l'adresse 0.0.0.0 et le port 5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
