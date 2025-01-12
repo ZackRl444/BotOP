aa55219c397ec7bdaa75be015164c15d372dff1c
ZackRl444 <zigzag140708@gmail.com>
2025-01-12 14:55:08 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from threading import Thread
 
@@ -13,20 +12,4 @@ def run():
 
 def keep_alive():
     t = Thread(target=run)
-
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Le bot est en ligne !"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-
     t.start()
